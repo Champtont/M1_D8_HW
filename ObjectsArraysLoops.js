4d9ef837ddfd25ce8a1b121ffe426336fbd919f3
Courtney Hampton <jaye0888@gmail.com>
2022-10-06 17:16:14 +0200

    Update ObjectsArraysLoops.js
    update

--- ObjectsArraysLoops.js
+++ ObjectsArraysLoops.js
@@ -97,6 +97,9 @@ const blkFriCart = totalShoppingCart - percentage;
 console.log(blkFriCart);
 */
 
+//or
+/*const discount = (totalShoppingCart / 100) * 20*/
+
 /* EXERCISE 8
  Create a variable and assign to it an object representing a car, with properties like brand, model and licensePlate.
  Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
